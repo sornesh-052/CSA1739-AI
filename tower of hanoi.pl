7251aa6c9b057307c1360ae28afcf2f9dbbd5605
sornesh-052 <123526385+sornesh-052@users.noreply.github.com>
2024-08-05 19:59:18 +0530

    Update tower of hanoi.pl

--- tower of hanoi.pl
+++ tower of hanoi.pl
@@ -1,13 +1,24 @@
-move(1, X, Y, _) :-
-    write('Move top disk from '),
-    write(X),
-    write(' to '),
-    write(Y),
-    nl.
+hanoi(1, Source, _, Destination) :-
 
-move(N, X, Y, Z) :-
-    N > 1,
-    M is N-1,
-    move(M, X, Z, Y),
-    move(1, X, Y, _),
-    move(M, Z, Y, X).
+    write('Move disk from '), write(Source),
+    write(' to '), write(Destination), nl.
+hanoi(N, Source, Auxiliary, Destination) :-
+
+    M is N - 1,
+    hanoi(M, Source, Destination, Auxiliary),
+
+    hanoi(1, Source, _, Destination),
+
+    hanoi(M, Auxiliary, Source, Destination).
+
+OUTPUT:
+?-hanoi(3).
+
+Move disk from A to C
+Move disk from A to B
+Move disk from C to B
+Move disk from A to C
+Move disk from B to A
+Move disk from B to C
+Move disk from A to C
+true 
